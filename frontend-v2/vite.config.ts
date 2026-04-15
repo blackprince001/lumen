f50a50737002511d6247c3a12aeb3d18ac364b43
blackprince001 <appiahboaduprince@gmail.com>
2026-04-15 13:49:26 +0000

    fix: allow all host fix

--- frontend-v2/vite.config.ts
+++ frontend-v2/vite.config.ts
@@ -1,26 +1,21 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const frontend = env.FRONTEND_DOMAIN;
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    server: {
-      host: '0.0.0.0',
-      port: 5173,
-    },
-    preview: {
-      host: '0.0.0.0',
-      port: 4173,
-      allowedHosts: [frontend],
-    },
-  };
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: true,
+  },
 });
