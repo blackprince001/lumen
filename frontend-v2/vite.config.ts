9f69a5b6e8a80178558395afa9f2627e10283402
blackprince001 <appiahboaduprince@gmail.com>
2026-04-15 13:40:57 +0000

    frontend domain in config

--- frontend-v2/vite.config.ts
+++ frontend-v2/vite.config.ts
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const frontend = env.FRONTEND_DOMAIN;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
-    allowedHosts: ['testing.maurc.org'],
-  },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: 4173,
+      allowedHosts: [frontend],
+    },
+  };
 });
